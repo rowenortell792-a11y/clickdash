2a08905304f2b78b9a273f0389f00f78210a685a
GGC Holdings <rowenortell792@gmail.com>
2026-04-30 02:34:13 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,13 +1,1 @@
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema({
-  discordId: { type: String, required: true, unique: true },
-  username: String,
-  avatar: String,
-  orbsBalance: { type: Number, default: 0 },
-  memberSince: { type: String, default: "Feb 23, 2026" },
-  systemStatus: { type: String, default: "ALPHA-PRODIGY" },
-  lastActive: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("User", UserSchema);
+plan: { type: String, default: "free" },  // ← add this
