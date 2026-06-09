3aee505e2d978d96d74c1e62dd68cad636b265c0
GGC Holdings <rowenortell792@gmail.com>
2026-06-08 22:09:29 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+# DO NOT HARDCODE KEYS HERE.
+# Use environment variables so they stay hidden in Railway/Server memory.
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+ARCHITECT_KEY = os.getenv("ARCHITECT_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
